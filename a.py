c079a2da66f971b07a97389e90625c34bc6ec88b
Sebastien Huber <sebastien_huber@laposte.net>
2017-04-02 11:29:56 -0700

    added -v -f -s

--- a.py
+++ a.py
@@ -3,6 +3,9 @@
 import argparse
 
 parser=argparse.ArgumentParser()
-parser.parse_args()
 
-parser.add_argument
+parser.add_argument("-v", "--verbose", help="display more informations", action="store_true")
+parser.add_argument("-s", "--srv", help="to specify one or more servers. For more srv, use: -s \"srv1 srv2 srv3...\"", type=str)
+parser.add_argument("-f", "--file", help="specify a source file. Each line containing a server name")
+
+parser.parse_args()
