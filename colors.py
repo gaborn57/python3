cc5d18ebeda15030f1da7d3ce7757a8274789b53
Sebastien Huber <gaborn57@gmail.com>
2017-04-15 19:15:23 +0200

    Create colors.py
    contains usefull color definitions, for python use in text mode

---
+++ colors.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+# -*- encoding: utf-8; py-indent-offset: 2 -*-
+
+colorlist=('bgrey', 'blgrey', 'bred', 'blred', 'bgreen', 'blgreen', 'byellow', 'bblue', 'blblue', 'bmagenta', 'blmagenta', 'bcyan', 'blcyan', 'bwhite', 'grey','lgrey', 'red', 'lred', 'green', 'lgreen', 'yellow', 'blue', 'lblue', 'magenta','lmagenta', 'cyan', 'lcyan', 'white')
+
+bgrey    ='\033[1m\033[30m'
+blgrey   ='\033[1m\033[90m'
+bred     ='\033[1m\033[31m'
+blred    ='\033[1m\033[91m'
+bgreen   ='\033[1m\033[32m'
+blgreen  ='\033[1m\033[92m'
+byellow  ='\033[1m\033[93m'
+bblue    ='\033[1m\033[34m'
+blblue   ='\033[1m\033[94m'
+bmagenta ='\033[1m\033[35m'
+blmagenta='\033[1m\033[95m'
+bcyan    ='\033[1m\033[36m'
+blcyan   ='\033[1m\033[96m'
+bwhite   ='\033[1m\033[97m'
+
+grey    ='\033[30m'
+lgrey   ='\033[90m'
+red     ='\033[31m'
+lred    ='\033[91m'
+green   ='\033[32m'
+lgreen  ='\033[92m'
+yellow  ='\033[93m'
+blue    ='\033[34m'
+lblue   ='\033[94m'
+magenta ='\033[35m'
+lmagenta='\033[95m'
+cyan    ='\033[36m'
+lcyan   ='\033[96m'
+white   ='\033[97m'
+
+warning  =yellow
+critical =red
+bwarning =byellow
+bcritical=bred
+reset    ='\033[0m'
