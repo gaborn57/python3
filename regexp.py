bced8c1a3572d1b7c0d76b82078e6128fd1e601a
sebhube <sebastien.huber@daimler.com>
2017-05-09 19:42:12 +0200

    ajouté compile

--- regexp.py
+++ regexp.py
@@ -5,6 +5,14 @@ import re
 
 series='Miss Fisher:Phrany Fisher;GOT:Daenerys Targaryen;X-Files:Dana Scully;Miss Fisher:Jack Robinson;Miss Fisher:Dorothy Williams;GOT:Jon Snow;GOT:Tyrion Lannister;X-Files:Fox Mulder'
 
-pattern=r':([a-zA-Z ]+)[;|$]'
+# on donne au findall le pattern et la source. Il génère un tableau
+pattern=r':([a-zA-Z ]+)[;$]'
 match=re.findall(r'GOT'+pattern, series)
+
+# ou alors on applique un findall, mais cette fois sur un objet compilé
+pattern2=re.compile(r'GOT:([A-Za-z ]+)[;$]')
+match2=pattern2.findall(series)
+
+# et les résultats sont identiques
 print(match)
+print(match2)
